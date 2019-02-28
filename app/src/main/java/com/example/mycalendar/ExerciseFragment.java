decb000da61a73b6d7c040b55c2106f2171fd0d5
Wenyu Ouyang <wenyuo@uci.edu>
2019-02-28 13:18:32 -0800

    Merge branch 'master' of /Users/wenyuouyang/AndroidStudioProjects/Ladybug with conflicts.

--- app/src/main/java/com/example/mycalendar/ExerciseFragment.java
+++ app/src/main/java/com/example/mycalendar/ExerciseFragment.java
@@ -1,5 +1,6 @@
 package com.example.mycalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class ExerciseFragment extends Fragment {
 
@@ -15,4 +17,17 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_exercise,container,false);
     }
+
+    public void sendHome(View view){
+        Intent Home = new Intent(getActivity(),MainActivity.class);
+        startActivity(Home);
+    }
+
+    ImageButton ib =(ImageButton)myView.findViewById(R.id.img_btn);
+    ib.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(getActivity(), yourNewActivity.class)) ;
+        }
+    });
 }
